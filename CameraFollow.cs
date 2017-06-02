c03a0a869135f3bf9a5316b5ff32caf3f6b60ffd
HUAWEI_2017\Camilo <cyateya35439@universidadean.edu.co>
2017-06-02 13:09:22 -0500

    todo el codigo de la camara

---
+++ CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;//el objeto que queremos que la camara siga
+    public float smoothing = 5f;//indicar el nivel o la suavidad que valla siguiendo al personaje
+    Vector3 offset;  // esta variable tendra el vector que separa al persona de la camara
+
+
+    //metodo
+    void Start() {
+
+        offset = transform.position - target.position;
+
+    
+    }
+
+    void FixedUpdate() {
+
+        Vector3 targetCamPos = target.position + offset; /// variable que va a indicar el punto de destino donde queremos poner la camara
+
+        //iremos avanzando los fotoramas de poco a poco 
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);//actualizar la posicion de la camara // Lerp() interpolacion 
+    }
+}
---
+++ EnemyAttack.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;//MUY IMPORTANTE !!! EL TIEMPO QUE TIENE QUE PASAR PARA QUE EL ENEMIGO VUELVA ATACAR
+    public int attackDamage = 5;//EL DAÑO QUE LE GENERA AL JUGADRO
+
+
+    Animator anim;// ANIMACION DE CAMBIO DE ESTAR MOVIENDOSE A ESTAR QUIETO 
+    GameObject player;//
+    PlayerHealth playerHealth;//LLAMARA AL METODO 
+    EnemyHealth enemyHealth;
+    bool playerInRange;//SABER SI EL JUGADOR VA A ESTAR AL LADO
+    float timer;//NOS PERMITE SABER EL TIEMPO DESDE QUE EL ENEMIGO ATACO
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent <Animator> ();
+    }
+
+
+    void OnTriggerEnter (Collider other)//
+        //CUANDO EL JUGADOR ESTA CERCA DEL ENEMIGO 
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+    // CUANDO EL JUGADOR YA NO ESTA CERCA EN EL RANGO DEL ENEMIGO
+    void OnTriggerExit (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+
+    void Update ()
+    {
+        timer += Time.deltaTime;//INCREMENTAMOS 
+
+        if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack ();
+        }
+
+        if(playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger ("JugadorMuerto");//HACE REFERENCIA A LA ANIMACION DEL ENEMIGO 
+        }
+    }
+
+
+    void Attack ()
+    {
+        timer = 0f;//reinicializar el componenete de tiempo 
+
+        if(playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage (attackDamage);
+        }
+    }
+}
